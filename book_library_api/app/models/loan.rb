06191452e407ca0828582ea3c784625b2795a86e
unknown <patrickkeita80@yahoo.com>
2024-12-30 00:08:09 -0800

    Add book model test

--- book_library_api/app/models/loan.rb
+++ book_library_api/app/models/loan.rb
@@ -1,3 +1,8 @@
 class Loan < ApplicationRecord
   belongs_to :book 
+
+  # Validations
+  validates :book_id, presence: true
+  validates :user_name, presence: true, length: { maximum: 255 }
+  validates :loaned_at, presence: true
 end
--- book_library_api/app/models/shelf.rb
+++ book_library_api/app/models/shelf.rb
@@ -1,3 +1,6 @@
 class Shelf < ApplicationRecord
   has_many :books, dependent: :destroy
+
+  # Validations
+  validates :name, presence: true, uniqueness: true, length: { maximum: 100 }
 end
